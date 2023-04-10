f5e3797741412c66a251e87f39c49a763cf48294
E Bolton <eebmagic@gmail.com>
2023-04-10 14:02:51 -0400

    add count for newbooks (last 48 hours)

--- count.py
+++ count.py
@@ -1,6 +1,8 @@
 import os
 import json
 from tqdm import tqdm
+import time
+
 
 def find_txt_files(dir_path):
     txt_files = []
@@ -26,9 +28,16 @@ for d in tqdm(lines):
 years = []
 counts = {}
 skiptotal = 0
+currtime = time.time()
+twodaytime = currtime - 172800 # Unix time 48 hours ago
+newtotal = 0
 print(f"wakling dir...")
 for path, ident, size in tqdm(find_txt_files('./downloads/')):
     if ident in table:
+        ctime = os.stat(path).st_ctime
+        if ctime > twodaytime:
+            newtotal += 1
+
         d = table[ident]
         if 'year' in d:
             y = table[ident]['year']
@@ -43,6 +52,7 @@ for year in sorted(counts.keys()):
     print(year, f"{str(counts[year]):<6}", '█' * int(50 * counts[year] / max(counts.values())))
 
 print(f"\nTotal skipped: {skiptotal}")
+print(f"Total books in last 48 hours: {newtotal}")
 print(f"Total books: {total}")
 
 import matplotlib.pyplot as plt
